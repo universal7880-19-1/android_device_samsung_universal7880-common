0c9435cbedb109fb88b41ae884cc4f93a87b98cd
Tim Zimmermann <tim@linux4.de>
2022-01-15 20:32:48 +0100

    universal7880: Uprev audio HAL to V7
    Updated audio policy by running:
    
    hardware/interfaces/audio/7.0/config/update_audio_policy_config.sh \
    device/samsung/universal7880-common/configs/audio/audio_policy_configuration.xml 6.0
    
    with some manual intervention.
    
    Change-Id: Iede32c176742e89577365341732a86f6ecaee514

--- common.mk
+++ common.mk
@@ -216,20 +216,20 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.common-util \
     android.hardware.audio.common@2.0-util \
     android.hardware.audio.common@2.0 \
-    android.hardware.audio.common@6.0-util \
-    android.hardware.audio.common@6.0 \
+    android.hardware.audio.common@7.0-util \
+    android.hardware.audio.common@7.0 \
     audio_amplifier.universal7880_32 \
     audio.primary.universal7880_32 \
     audio.a2dp.default \

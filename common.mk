d5b6ce561dfdfcb8250daa5e65ddd3c7304570f2
Tim Zimmermann <tim@linux4.de>
2022-01-15 20:41:38 +0100

    universal7880: switch to new AIDL vibrator HAL
    Change-Id: If10972cce06dd5128729d4d7409c57f79775b09b

--- common.mk
+++ common.mk
@@ -304,8 +304,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator-service.samsung
 
 # VNDK prebuilts
 PRODUCT_COPY_FILES += \

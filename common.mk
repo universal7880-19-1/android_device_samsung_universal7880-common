b335896f600743dbf118a8ef3f45fa66fefae747
ivanmeler <i_ivan@windowslive.com>
2022-01-15 20:12:10 +0100

    universal7880: explicitly build RIL HIDL dependencies
    * Required as of https://android.googlesource.com/platform/hardware/interfaces/+/b0907a6bb86e6ab67e181ac2477da8d827233d1f
    
    Change-Id: Ie22340d93419d6892eab353a6f9d9cdae8606c4e

--- common.mk
+++ common.mk
@@ -153,6 +153,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
     android.hardware.radio.config@1.2 \
+    android.hardware.radio.deprecated@1.0 \
     libxml2 \
     libprotobuf-cpp-full
 
